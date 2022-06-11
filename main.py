1cc7825ac058089af02a7581d0a331abf2a6c411
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-06-11 19:35:23 +0200

    Added a character counter

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def get_input(msg = "Enter something"):
     return str_input
 
 def main():
-    get_input("Please enter a pharse to decrypt")
+    str_to_decrypt = get_input("Please enter a pharse to decrypt")
+    char_count = {}
+    for i in str_to_decrypt:
+        if(i in char_count):
+            char_count[i] = 1
+        else:
+            char_count[i] = char_count[i] + 1
+    
 
 
 if(__name__ == "__main__"):
