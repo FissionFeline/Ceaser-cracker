a5acdda21b62ad22956c18fe3d4bab9ae5981abd
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-06-11 19:29:53 +0200

    Added main function

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ def get_input(msg = "Enter something"):
     if(len(str_input) < 3 or str_input.isspace()):
         return get_input("This requires valid input")
     return str_input
-    
-get_input("Please enter a pharse to decrypt")
+
+def main():
+    get_input("Please enter a pharse to decrypt")
+
+
+if(__name__ == "__main__"):
+    main()
